afb6b7e311c39ce3b4da97abf59e22c824b15009
loivis <github@kinase.wang>
2019-04-21 23:45:07 +0200

    wip: update text page

--- lib/screen/text.dart
+++ lib/screen/text.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pavium/model/favorite.dart';
 import 'package:pavium/store/book.dart';
 import 'package:pavium/store/favorite.dart';
+import 'package:pavium/util/paginator.dart';
 
 class TextPage extends StatefulWidget {
   final Favorite favorite;
@@ -33,9 +34,19 @@ class _TextPageState extends State<TextPage> {
             ),
           );
         }
+
+        Size size = MediaQuery.of(context).size;
+        print(size);
+
+        var _pages = Paginator.getPages(text, size.height, size.width, 25);
+        print(_pages.length);
+
         return Scaffold(
           body: Center(
-            child: Text(text.substring(0, 33)),
+            child: Text(
+              _pages[0],
+              style: TextStyle(fontSize: 20),
+            ),
           ),
         );
       },
